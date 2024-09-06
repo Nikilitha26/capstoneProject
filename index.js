db014c36ccacd2087be83c49c5502a1348ac5428
Nikilitha26 <kakazanikilitha544@gmail.com>
2024-09-06 11:21:13 +0200

    adding on index.js

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-import cors from 'cors'
-import express from 'express'
-import {config} from 'dotenv'
-import productsRouter from './routes/productsRouter.js'
-import usersRouter from './routes/usersRouter.js'
-import { isAdmin } from './middleware/authenticate.js'
+import cors from 'cors';
+import express from 'express';
+import { config } from 'dotenv';
+import productsRouter from './routes/productsRouter.js';
+import usersRouter from './routes/usersRouter.js';
+import { isAdmin } from './middleware/authenticate.js';
 
-config()
+config();
 
+let PORT = process.env.PORT || 5005;
+const app = express();
 
-let PORT = process.env.PORT || 5005
-const app = express()
 app.use(cors({
-    origin: ['http://localhost:8080', 'https://capstoneproject-1-9k8p.onrender.com'],
-    credentials: true
-  }))
-app.use(express.json())
-app.use(express.static('public'))
+  origin: ['http://localhost:8080', 'https://capstoneproject-1-9k8p.onrender.com'],
+  credentials: true
+}));
 
+app.use(express.json());
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-  })
-app.use('/users', usersRouter)
-app.use('/products', productsRouter) 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
 app.get('/admin', isAdmin, (req, res) => {
-    // Admin page logic here
-  });
-app.listen(PORT, ()=>{
-    console.log('http://localhost:'+PORT);
-})
+  // Admin page logic here
+});
+
+app.listen(PORT, () => {
+  console.log('http://localhost:' + PORT);
+});
 
 app.get('^/$|/capstoneaproject-backend', (req, res) => {
-    res.status(200).sendFile(path.resolve('./static/html/index.html'))
-})
+  res.status(200).sendFile(path.resolve('./static/html/index.html'));
+});
 
 app.get('*', (req, res) => {
-    res.json({
-        status: 404,
-        msg: 'Resource not found'
-    })
-})
+  res.json({
+    status: 404,
+    msg: 'Resource not found'
+  });
+});
+
